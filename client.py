533e9fff037a20a3fa18ce3f01a30ad41dba0c29
Your Name <you@example.com>
2025-05-12 18:08:27 +0200

    nothing really

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
- import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt
 import json
 import time
 from pipuck.pipuck import PiPuck
+import numpy as np
 
 # Define variables and callbacks
 Broker = "192.168.178.56"  # Replace with your broker address
@@ -20,6 +21,17 @@ def on_message(client, userdata, msg):
     except json.JSONDecodeError:
         print(f'invalid json: {msg.payload}')
 
+
+def got_to_point(position, orientation, point, eps=0.01):
+    delta = position - point
+    if np.linalg.norm(position - point) <= eps:
+        return True
+    if delta @ orientation <  0:
+        
+
+
+
+
 # Initialize MQTT client
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -32,12 +44,10 @@ client.loop_start() # Start listening loop in separate thread
 # Initialize the PiPuck
 pipuck = PiPuck(epuck_version=2)
 
-# Set the robot's speed, e.g. with
-pipuck.epuck.set_motor_speeds(1000,-1000)
-
-for _ in range(1000):
-    # TODO: Do your stuff here
-	time.sleep(1)
+for _ in range(10):
+    speed_l, speed_r = np.random.normal(size=2).clip(min=-1, max=1) * 100
+    pipuck.epuck.set_motor_speeds(speed_l, speed_r)
+    time.sleep(1)
 	
     
 # Stop the MQTT client loop
