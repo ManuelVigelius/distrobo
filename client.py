8c7d99a0c7af8f8eb853209ca5641a8749a4ba69
Your Name <you@example.com>
2025-05-13 12:03:04 +0200

    nothing really

--- client.py
+++ client.py
@@ -3,10 +3,15 @@ import json
 import time
 from pipuck.pipuck import PiPuck
 import numpy as np
+import math
 
 # Define variables and callbacks
 Broker = "192.168.178.56"  # Replace with your broker address
-Port = 1883 # standard MQTT port
+Port = 1883 # standard MQTT port, original 1883
+
+my_id = '2'
+position = 0
+orientation = 0
 
 # function to handle connection
 def on_connect(client, userdata, flags, rc):
@@ -17,19 +22,36 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     try:
         data = json.loads(msg.payload.decode())
-        print(data)
+        if my_id in data.keys():
+            position = np.asarray(data[my_id]['position'])
+            orientation = angle_to_orientation(data[my_id]['angle'])
+        else:
+            print('Err: no position')
+            print(data)
     except json.JSONDecodeError:
         print(f'invalid json: {msg.payload}')
 
+def orient_towards(position, orientation, point, eps=0.05, speed=200):
+    delta = position - point
+    if delta @ orientation >= 1 - eps:
+        return True
+    if delta[0] > delta[0]:
+        pipuck.epuck.set_motor_speeds(speed, -speed)
+    else:
+        pipuck.epuck.set_motor_speeds(-speed, speed)
+    return False
 
-def got_to_point(position, orientation, point, eps=0.01):
+def got_to_point(position, orientation, point, eps=0.01, speed=1000):
     delta = position - point
     if np.linalg.norm(position - point) <= eps:
         return True
-    if delta @ orientation <  0:
-        
+    if not(orient_towards(position, orientation, point)):
+        return False
+    pipuck.epuck.set_motor_speeds(speed, speed)
 
-
+def angle_to_orientation(angle):
+    angle_radians = math.radians(angle)
+    return np.asarray([math.cos(angle_radians), math.sin(angle_radians)])
 
 
 # Initialize MQTT client
@@ -41,12 +63,16 @@ client.connect(Broker, Port, 60)
 
 client.loop_start() # Start listening loop in separate thread
 
-# Initialize the PiPuck
 pipuck = PiPuck(epuck_version=2)
 
+
+goal = np.asarray([0, 0])
+
+for _ in range(30):
+    time.sleep(0.1)
+
 for _ in range(10):
-    speed_l, speed_r = np.random.normal(size=2).clip(min=-1, max=1) * 100
-    pipuck.epuck.set_motor_speeds(speed_l, speed_r)
+    got_to_point(position, orientation, goal)
     time.sleep(1)
 	
     
